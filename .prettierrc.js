aed1cbab6b9d89382fc0da2e370e655a031abd0d
Otávio <otavio@cargon.com.br>
2024-04-20 12:02:55 -0300

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: "always",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	singleQuote: false,
+	trailingComma: "all",
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	printWidth: 80,
+};
