4cf0782812a5c25f6efc95b3aa8dc676494400bf
Otávio <otavio@cargon.com.br>
2024-04-20 16:29:24 -0300

    feature: main func

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world')
+import * as core from "@actions/core"
+// import * as github from "@actions/github"
+
+function main(): void {
+	const notionSecret = core.getInput("NOTION_SECRET", { required: true })
+	const notionSage = core.getInput("NOTION_PAGE", { required: true })
+
+	core.info(notionSecret)
+	core.info(notionSage)
+}
+
+main()
