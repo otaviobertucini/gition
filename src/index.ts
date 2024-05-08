1ce2d8741f91e3f9ab6161e01b119eba18b0fdaf
Otávio <otavio@cargon.com.br>
2024-05-08 20:57:12 -0300

    feature: fetch and convert pages

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,54 @@
 import * as core from "@actions/core"
+import { Client } from "@notionhq/client"
+import dotenv from "dotenv"
+import { NotionToMarkdown } from "notion-to-md"
+dotenv.config()
 // import * as github from "@actions/github"
 
-function main(): void {
-	const notionSecret = core.getInput("NOTION_SECRET", { required: true })
-	const notionSage = core.getInput("NOTION_PAGE", { required: true })
-
-	core.info(notionSecret)
-	core.info(notionSage)
+interface Credentials {
+	notionSecret: string
+	notionPage: string
 }
 
-main()
+function getDevelopmentCredentials(): Credentials {
+	return {
+		notionPage: process.env.NOTION_PAGE ?? "",
+		notionSecret: process.env.NOTION_SECRET ?? "",
+	}
+}
+
+function getActionCredentials(): Credentials {
+	return {
+		notionPage: core.getInput("NOTION_SECRET", { required: true }),
+		notionSecret: core.getInput("NOTION_PAGE", { required: true }),
+	}
+}
+
+async function main(): Promise<void> {
+	let credentials: Credentials
+
+	const args = process.argv.slice(2)
+	if (args.includes("--development")) {
+		credentials = getDevelopmentCredentials()
+	} else {
+		credentials = getActionCredentials()
+	}
+
+	core.info(JSON.stringify(credentials))
+
+	const notion = new Client({ auth: credentials.notionSecret })
+
+	const notion2md = new NotionToMarkdown({
+		notionClient: notion,
+		config: {
+			// parseChildPages: false,
+			separateChildPage: true,
+		},
+	})
+
+	const mdBlocks = await notion2md.pageToMarkdown(credentials.notionPage)
+	const content = notion2md.toMarkdownString(mdBlocks)
+	console.log(`🚀 ~ main ~ content:`, content)
+}
+
+main().catch(() => {})
